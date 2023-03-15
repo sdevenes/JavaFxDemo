cdb274f9aac29f34dde273aa8eb3ccf4ab5bbb52
SteveDevenes <steve.devenes@hevs.ch>
2023-03-15 14:39:49 +0100

    CHG: dummy changes

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,9 +6,13 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label mylabel;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        mylabel.setText("Helloooooo");
     }
 }
